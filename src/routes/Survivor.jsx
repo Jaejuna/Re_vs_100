730aadb15fd52c1edb2238e1b8037c12c662e6b6
Sooyoung <dannylisa67@gmail.com>
2021-04-02 01:48:15 +0900

    Survivor Designed

--- src/routes/Survivor.jsx
+++ src/routes/Survivor.jsx
@@ -7,31 +7,19 @@ import { faCrown } from '@fortawesome/free-solid-svg-icons';
 import Btn from '../materials/Btn'
 
 const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     font-size: 2em;
     font-weight: bolder;
     text-align: center;
     align-items: center;
     letter-spacing: 1.5px;
+    color: #dbe2fd;
 `
-const Crown = styled.div`
-    color: white;
-`
-
-const Surv = styled.div`
-    font-style: italic;
-`
-
-const TextColor =styled.div`
-    display: inline-grid;
-    grid-template-rows: 200px auto auto 100px;
-    background: linear-gradient(to bottom, #fce4ec, #e3f2fd);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    ${media.tablet`
-    grid-template-rows: 100px auto auto 100px;
-    font-size: 0.6em;
-    `}
+const Winner = styled.div`
+    display: grid;
+    grid-template-columns: 50px 200px 50px;
 `
 
 const Survivor = ({userObj}) => {
@@ -42,29 +30,35 @@ const Survivor = ({userObj}) => {
         dbService.collection("users").onSnapshot( snapshot => {
             const surv = snapshot.docs.map( doc => doc.data())
             .filter(a => a.available ===true)
-            .map( b => b.alias).map((x,i,arr)=>(i<arr.length-1)?x+', ':x);
+            .map( b => b.alias);
             setLastSurv(surv);
         })
     }, []);
 
-    const onClickToDraw = async() => {
-        await dbService.collection('current').doc('current').update({
+    const onClickToDraw = () => {
+        dbService.collection('current').doc('current').update({
             isDraw : true
         })
     }
 
     return (
         <Wrapper>
-            <TextColor>
-            <div>
-                마지막까지 생존하신걸 축하드립니다!!!<br/>
-                문자가 발송될 예정이니 꼭 확인해주세요 :) 
-            </div>
-            <Crown>
-                <FontAwesomeIcon icon={faCrown} /> 생존자 명단 <FontAwesomeIcon icon={faCrown} />
-            </Crown>
-            <Surv>{[lastSurv]}</Surv>
-            </TextColor>
+            <p>
+                최후의 {lastSurv.length}인
+            </p>
+            {
+                lastSurv.map((surv, idx) => (
+                    <Winner key={idx}>
+                        <FontAwesomeIcon icon={faCrown}/>
+                            &nbsp;{surv}&nbsp;
+                        <FontAwesomeIcon icon={faCrown} />
+                    </Winner>
+                ))
+            }
+            <p>
+                문자가 발송될 예정이니 꼭 확인해주세요!
+            </p>
+
             { isAdmin &&
                 <Btn onClick = {onClickToDraw}> 추첨 하러 가기 </Btn>
             }
